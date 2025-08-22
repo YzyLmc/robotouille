f4c970cd70409d95e44e2c7eecc4cbc9a938f8a6
YzyLmc <yzy729600559@hotmail.com>
2025-08-22 02:38:39 -0400

    Add data collection script

---
+++ run_skill_sequence.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append("/home/ziyi/git/skill_wrapper/src")
+import argparse
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from robotouille.robotouille_env import create_robotouille_env
+from skills import SkillManager, run_skill_sequence_and_record
+from src.utils import load_from_file
+
+def exec_and_record(environment_name: str, skill_sequence_fpath: str, args, **kwargs):
+    '''Minimal script for testing action rollout and screen shot'''
+    # Initialize environment
+    seed = kwargs.get('seed', None)
+    env = create_robotouille_env(environment_name, seed)
+    obs, info = env.reset()
+
+    # Initialize skill manager
+    skill_manager = SkillManager(env)
+    skill_sequence = load_from_file(skill_sequence_fpath)
+    # Run skill sequence
+    run_skill_sequence_and_record(skill_manager, skill_sequence, args.save_path)
+
+@hydra.main(version_base=None, config_path="conf", config_name="test_config")
+def main(cfg: DictConfig) -> None:
+    if not cfg.evaluation.evaluate:
+        kwargs = OmegaConf.to_container(cfg.game, resolve=True)
+        kwargs['llm_kwargs'] = OmegaConf.to_container(cfg.llm, resolve=True)
+        environment_name = kwargs.pop('environment_name')
+        exec_and_record(environment_name, args, **kwargs)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("skill_sequence_fpath", type=str, default="task_config/burger_skill_sequence.yaml", help="Path to the skill sequence YAML file.")
+    parser.add_argument("save_path", type=str, default="test_run/burger/", help="Path to save the execution results.")
+    args = parser.parse_args()
+
+    main()
